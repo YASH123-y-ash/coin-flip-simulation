2b8337239778ff1623f3926515d9060f34572397
Yash-y-ash <wardhan696@gmail.com>
2021-03-03 00:36:15 +0530

    Added:if score is equal flip a coin till the difference of score is 2

--- coinFlipSimulation.sh
+++ coinFlipSimulation.sh
@@ -9,6 +9,8 @@ else
 let tcount++
 fi
 done
+
+
 echo "head score::$hcount"
 echo "tail score::$tcount"
 if [ $hcount -gt $tcount ];then
@@ -17,6 +19,24 @@ echo "Head wins by $(($hcount-$tcount)) points from Tail"
 elif [ $hcount -lt $tcount ];then
 echo "Tail wins total score is: $tcount"
 echo "Tail wins by $(($tcount-$hcount)) point from Head"
+fi
+
+
+if [ $hcount -eq $tcount ];then
+echo "its a tie match"
+while [ $tcount -lt $(($hcount+2)) ] && [ $hcount -lt $(($tcount+2)) ]
+do
+if [ $((RANDOM%2)) -eq 1 ];then
+let hcount++
 else
-echo "Its a tie match:: Head score:$hcount :: Tail score:$tcount"
+let tcount++
+fi
+done
+fi
+
+
+if [ $hcount -eq 23 ];then
+echo "head score is $hcount and tail score is $tcount and difference of score is $(($hcount-$tcount))"
+elif [ $tcount -eq 23 ];then
+echo "head score is $hcount and tail score is $tcount and difference of score is $(($tcount-$hcount))"
 fi
