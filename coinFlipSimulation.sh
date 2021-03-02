419cdb176832abe51ef6dea94cc3b3204bcebbae
Yash-y-ash <wardhan696@gmail.com>
2021-03-02 23:23:33 +0530

    Added:displaying winner between head and tail and winning difference

--- coinFlipSimulation.sh
+++ coinFlipSimulation.sh
@@ -1,18 +1,22 @@
-#!/bin/bash -x
-totalcount=100
+#!/bin/bash
 hcount=0
 tcount=0
-while [ $totalcount -gt 0 ]
+while [ $tcount -lt 21  ] && [ $hcount -lt 21 ]
 do
-if [ $((RANDOM%2)) -eq 1 ]
-then
+if [ $((RANDOM%2)) -eq 1 ];then
 let hcount++
 else
 let tcount++
 fi
-let totalcount--
 done
-echo "tail wins $tcount times"
-echo "head wins $hcount times"
-
-
+echo "head score::$hcount"
+echo "tail score::$tcount"
+if [ $hcount -gt $tcount ];then
+echo "Head wins total score is: $hcount"
+echo "Head wins by $(($hcount-$tcount)) points from Tail"
+elif [ $hcount -lt $tcount ];then
+echo "Tail wins total score is: $tcount"
+echo "Tail wins by $(($tcount-$hcount)) point from Head"
+else
+echo "Its a tie match:: Head score:$hcount :: Tail score:$tcount"
+fi
